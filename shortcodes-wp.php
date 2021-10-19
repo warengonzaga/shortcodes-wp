dc60b4cff799c9e792549766f6914821697536ce
Christof Lee <christoflee63@gmail.com>
2021-10-19 21:04:17 +0100

    ✨ TWEAK: namespace for shortcode (#8)
    * Current day shortcode
    
    * Current month shortcode
    
    * Current year shortcode
    
    * Current date shortcode
    
    - Give the user some control over the format.
    
    * Get currrent date, not date of post
    
    * Add shortcode for query string
    
    - Pass the shortcode an arg, the parameter of the query string.
    - Clean and return it.
    
    * Query string => query paran
    
    * Check function exists before defining one
    
    - Avoid conflicts
    
    * Fix name of shortcode

--- shortcodes-wp.php
+++ shortcodes-wp.php
@@ -15,66 +15,79 @@
 // prevent direct access
 defined( 'ABSPATH' ) or die( "Restricted Access!" );
 
-function user_firstname($atts) {
-    $user = wp_get_current_user();
-    $firstname = $user->first_name;
-    $display_firstname = $firstname;
+if (!function_exists('wpuser_firstname_shortcode')){
+    function wpuser_firstname_shortcode($atts) {
+        $user = wp_get_current_user();
+        $firstname = $user->first_name;
+        $display_firstname = $firstname;
 
-    // display output
-    return $display_firstname;
+        // display output
+        return $display_firstname;
+    }
 }
 
-function user_lastname($atts) {
-    $user = wp_get_current_user();
-    $lastname = $user->last_name;
-    $display_lastname = $lastname;
+if (!function_exists('wpuser_lastname_shortcode')){
+    function wpuser_lastname_shortcode($atts) {
+        $user = wp_get_current_user();
+        $lastname = $user->last_name;
+        $display_lastname = $lastname;
 
-    // display output
-    return $display_lastname;
+        // display output
+        return $display_lastname;
+    }
 }
 
 // wordpress hook
-add_shortcode('wpuser_firstname', 'user_firstname');
-add_shortcode('wpuser_lastname', 'user_lastname');
+add_shortcode('wpuser_firstname', 'wpuser_firstname_shortcode');
+add_shortcode('wpuser_lastname', 'wpuser_lastname_shortcode');
 
 // Current day.
-add_shortcode('wpinfo_current_day', 'current_day');
-function current_day() {
-    return gmdate( 'l' );
+add_shortcode('wpinfo_current_day', 'wp_info_current_day_shortcode');
+if (!function_exists('wp_info_current_day_shortcode')){
+    function wp_info_current_day_shortcode() {
+        return gmdate( 'l' );
+    }
 }
 
 // Current month.
-add_shortcode('wpinfo_current_month', 'current_month');
-function current_month() {
-    return gmdate( 'F' );
+add_shortcode('wpinfo_current_month', 'wp_info_current_month_shortcode');
+if (!function_exists('wp_info_current_month_shortcode')){
+    function wp_info_current_month_shortcode() {
+        return gmdate( 'F' );
+    }
 }
 
 // Current year.
-add_shortcode('wpinfo_current_year', 'current_year');
-function current_year() {
-    return gmdate( 'Y' );
+add_shortcode('wpinfo_current_year', 'wp_info_current_year_shortcode');
+if (!function_exists('wp_info_current_year_shortcode')){
+    function wp_info_current_year_shortcode() {
+        return gmdate( 'Y' );
+    }
 }
-
 /**
  * Current date
  *
  * – Allow the user to select their own format.
  * – Else fallback to the default option.
  */
-add_shortcode('wpinfo_current_date', 'current_date');
-function current_date($atts) {
-    $default_date_format = get_option( 'date_format' );
-    $atts = shortcode_atts( array(
-        'format' => $default_date_format,
-    ), $atts );
-    return gmdate( $atts['format'] );
+add_shortcode('wpinfo_current_date', 'wp_info_current_date_shortcode');
+if (!function_exists('wp_info_current_date_shortcode')){
+    function wp_info_current_date_shortcode($atts) {
+        $default_date_format = get_option( 'date_format' );
+        $atts = shortcode_atts( array(
+            'format' => $default_date_format,
+        ), $atts );
+        return gmdate( $atts['format'] );
+    }
 }
 
 // query param shortcode
-add_shortcode('wpuser_query_param', 'get_query_param');
-function get_query_param($atts){
-    $atts = shortcode_atts( array(
-        'arg' => false,
-    ), $atts);
-    return isset( $_GET[ $atts['arg'] ] ) ? sanitize_text_field( $_GET[ $atts['arg'] ] ) : false;
+add_shortcode('wpinfo_query_param', 'wp_info_get_query_param_shortcode');
+if (!function_exists('wp_info_get_query_param_shortcode')){
+    function wp_info_get_query_param_shortcode($atts){
+        $atts = shortcode_atts( array(
+            'arg' => false,
+        ), $atts);
+        return isset( $_GET[ $atts['arg'] ] ) ? sanitize_text_field( $_GET[ $atts['arg'] ] ) : false;
+    }
 }
